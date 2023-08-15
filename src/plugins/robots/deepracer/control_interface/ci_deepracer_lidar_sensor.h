4da0c46a754047db2b79c1f50be6cd6bf26a4fe6
Khai Yi Chin <khaiyichin@gmail.com>
2023-08-15 16:18:27 -0400

    Minor updates to header file preprocessor directives to maintain consistency.

--- src/plugins/robots/deepracer/control_interface/ci_deepracer_lidar_sensor.h
+++ src/plugins/robots/deepracer/control_interface/ci_deepracer_lidar_sensor.h
@@ -1,5 +1,5 @@
-#ifndef CI_DEEPRACER_LIDAR_SENSOR_H
-#define CI_DEEPRACER_LIDAR_SENSOR_H
+#ifndef CCI_DEEPRACER_LIDAR_SENSOR_H
+#define CCI_DEEPRACER_LIDAR_SENSOR_H
 
 namespace argos {
     class CCI_DeepracerLIDARSensor;
@@ -9,10 +9,10 @@ namespace argos {
 #include <argos3/core/utility/math/angles.h>
 #include <argos3/core/utility/math/vector3.h>
 
-
 namespace argos {
     class CCI_DeepracerLIDARSensor : public CCI_Sensor {
     public:
+
         /**
          * Class constructor
          */
@@ -23,21 +23,17 @@ namespace argos {
          */
         virtual ~CCI_DeepracerLIDARSensor() {}
 
-
         /**
          * Returns the readings of this sensor
          */
         virtual CVector3 GetReading() const = 0;
 
-
-
 #ifdef ARGOS_WITH_LUA
-      virtual void CreateLuaState(lua_State* pt_lua_state) {};
+        virtual void CreateLuaState(lua_State* pt_lua_state){};
 
-      virtual void ReadingsToLuaState(lua_State* pt_lua_state) {};
+        virtual void ReadingsToLuaState(lua_State* pt_lua_state){};
 #endif
-
     };
 }
 
-#endif //ARGOS3_DEEPRACER_CI_DEEPRACER_LIDAR_SENSOR_H
+#endif // CCI_DEEPRACER_LIDAR_SENSOR_H
